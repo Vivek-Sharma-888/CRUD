1bb61d2461b20efbf92ac053fe390743b8354ddb
VIVEK KUMAR <yvksharma@gmail.com>
2023-08-08 12:44:11 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = require("./router/router");
 const app = express();
 const cookieParser = require("cookie-parser")
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 require('../server/db/connect')
 const cors = require("cors")
 
